d020420e76ee07c5cbcefa01786181edf12e1377
TCS_Pablo Alonso Salas Alvarez <xt0179@rimac.com.pe>
2018-03-28 12:04:50 -0500

    update

---
+++ src/com/lucky/dto/Periodo.java
@@ -0,0 +1,55 @@
+package com.lucky.dto;
+
+import java.sql.Date;
+
+public class Periodo {
+	public Integer id;
+	public Integer anio;
+	public Integer mes;
+	public Integer semana;
+	public String nombre;
+	public Date fechaInicio;
+	public Date fechaFin;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getAnio() {
+		return anio;
+	}
+	public void setAnio(Integer anio) {
+		this.anio = anio;
+	}
+	public Integer getMes() {
+		return mes;
+	}
+	public void setMes(Integer mes) {
+		this.mes = mes;
+	}
+	public Integer getSemana() {
+		return semana;
+	}
+	public void setSemana(Integer semana) {
+		this.semana = semana;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+}
---
+++ src/com/lucky/dto/VisitaPdv.java
@@ -0,0 +1,30 @@
+package com.lucky.dto;
+
+public class VisitaPdv {
+	private Integer id;
+	private Visita visita;
+	private Pdv pdv;
+	public VisitaPdv(){
+		visita = new Visita();
+		pdv = new Pdv();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Visita getVisita() {
+		return visita;
+	}
+	public void setVisita(Visita visita) {
+		this.visita = visita;
+	}
+	public Pdv getPdv() {
+		return pdv;
+	}
+	public void setPdv(Pdv pdv) {
+		this.pdv = pdv;
+	}
+	
+}
