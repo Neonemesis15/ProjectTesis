54290d3fd0dfa57afbeb7bc7bd46262aa8b6ad97
Pablo Salas Alvarez <pablo.salas.alvarez88@gmail.com>
2018-06-13 07:00:09 -0500

    update

--- WebContent/js/cronograma.js
+++ WebContent/js/cronograma.js
@@ -1,4 +1,5 @@
 $(function(){
+	//Cargar Campanias Publicitarias
 	$.ajax({
 			url :"CampaniaPublicitaria",
 			data:{
@@ -21,7 +22,28 @@ $(function(){
 				
 			}
 	});
-	
+
+	//Cargar Tipos de Punto de Venta
+	$.ajax({
+		url:"TipoPdv",
+		data:{
+			accion:"CBO"
+		},
+		success: function(data){
+			var msg = $(data).find('msg').text();
+			if($.trim(msg).length !== 0){
+				message("Data no Encontrada", msg);
+			}else{
+				var option = "";
+				$(data).find('op').each(function(){
+					option += "<option value=\""
+					+ $(this).attr('id') + "\">"
+					+ $(this).text() + "</option>";
+				});
+				$("#idtippdv_ins").html(option);
+			}
+		}
+	});
 });
 
 function periodoCbo(){
@@ -85,16 +107,206 @@ function cronogramaQry(){
 }
 
 function cronogramaIns(){
-	$("#dlg_cronograma_ins").dialog({
-		modal: true,
-		width: 480,
-		buttons:{
-			"Cancelar": function(){
-				$(this).dialog("close");
+	// lectura del combo de campanias
+	$.ajax({
+			url :"CampaniaPublicitaria",
+			type:"post",
+			datatype:"xml",
+			data:{
+				accion:"CBO"
 			},
-			"Enviar Datos": function(){
-				
+			success: function(data){
+				var msg = $(data).find('msg').text();
+				if($.trim(msg).length !== 0){
+					message("Data no Encontrada", msg);
+				}else{
+					var option = "";
+					$(data).find('op').each(function(){
+						option += "<option value=\""
+						+ $(this).attr('id') + "\">"
+						+ $(this).text() + "</option>";
+					});
+					$('#idcampania_ins').html(option);
+
+					$.ajax({
+							url:"Periodo",
+							data:{
+								accion:"CBO_02",
+								idCampania: $("#idcampania").val()
+							},
+							success: function(data){
+								var msg = $(data).find('msg').text();
+								if($.trim(msg).length !== 0){
+									message("Data no Encontrada", msg);
+								}else{
+									var option = "";
+									$(data).find('op').each(function(){
+										option += "<option value=\""
+										+ $(this).attr('id')+"\">"
+										+ $(this).text() + "</option>";
+									});
+									$("#idperiodo_ins").html(option);
+								}
+							}
+					})
+
+
+
+
+
+
+					$("#dlg_cronograma_ins").dialog({
+						modal: true,
+						width: 480,
+						buttons:{
+							"Cancelar": function(){
+								$(this).dialog("close");
+							},
+							"Enviar Datos": function(){
+								
+							}
+						}
+					});
+
+
+
+
+
+
+
+
+				}
+			}
+	})
+
+
+
+}
+
+
+
+function periodoCboIns(){
+	$.ajax({
+		url:"Periodo",
+		data:{
+			accion:"CBO_02",
+			idCampania: $("#idcampania_ins").val()
+		},
+		success: function(data){
+			var msg = $(data).find("msg").text();
+			if($.trim(msg).length !== 0){
+				message("Data no Encontrada", msg);
+			}else{
+				var option = "";
+				$(data).find('op').each(function(){
+					option += "<option value=\""
+					+ $(this).attr("id")+"\">"
+					+ $(this).text() + "</option>";
+				});
+				$("#idperiodo_ins").html(option);
 			}
 		}
 	});
+}
+
+function usuariosFind(){
+	$("#dlg_usuarios_find").dialog({
+		modal:true,
+		width: 360,
+		height: 330,
+		buttons: {
+			"Cerrar":function(){
+				$(this).dialog("close");
+			}
+		}
+	});
+	$.ajax({
+		url: "Usuario",
+		type: "post",
+		data :{
+			accion : "QRY_02",
+			idCampania: $("#idcampania_ins").val(),
+			idPeriodo: $("#idperiodo_ins").val()
+		},
+		success: function(data){
+			var msg = $(data).find("msg").text();
+			if($.trim(msg).length !== 0){
+				message("Data no Encontrada", msg);
+			}else{
+				var lista = "<table class=\"parainfo\" style=\"100%\"><tbody>";
+				$(data).find('fil').each(function(){
+					var idUsuario = $(this).find('col:eq(0)').text();
+					var nomUsuario = $(this).find('col:eq(1)').text();
+
+					lista += "<tr><td>"
+					+ "<a class=\"parainfo\" href=\"#\" onclick=\"usuarioPinta('"+idUsuario+"','"+nomUsuario+"');\">"
+					+ nomUsuario
+					+ "</a>"
+					+ "</td></tr>";
+				});
+
+				lista += "</body></table>";
+
+				//Pinta Lista de Usuarios By IdCampania & IdPeriodo
+				$("#usuariosLst").html(lista);
+			}
+		}
+	});
+}
+
+function usuarioPinta(idUsuario, nomUsuario){
+	$("#idUsuario_ins").val(idUsuario);
+	$("#usuario_ins").val(nomUsuario);
+	//Cierra dialog
+	$("#dlg_usuarios_find").dialog("close");
+}
+
+
+function ubigeosFind(){
+	$("#dlg_ubigeo_find").dialog({
+		modal: true,
+		width: 360,
+		height: 330,
+		buttons: {
+			"Cerrar": function(){
+				$(this).dialog("close");
+			}
+		}
+	});
+
+	$.ajax({
+		url: "Ubigeo",
+		type:"post",
+		data: {
+			accion: "QRY_02",
+			idCampania: $("#idcampania_ins").val(),
+			idPeriodo: $("#idperiodo_ins").val(),
+			idTipPdv: $("#idtippdv_ins").val()
+		},
+		success: function(data){
+			var msg = $(data).find("msg").text();
+			if($.trim(msg).length !== 0){
+				message("Data no Encontrada", msg);
+			}else{
+				var lista = "<table class=\"parainfo\" style=\"100%\"><tbody>";
+				$(data).find('fil').each(function(){
+					var idUbigeo = $(this).find('col:eq(0)').text();
+					var nomUbigeo = $(this).find('col:eq(1)').text();
+					lista += "<tr><td>"
+					+ "<a class=\"parainfo\" href=\"#\" onclick=\"ubigeoPinta('"+idUbigeo+"','"+nomUbigeo+"');\">"
+					+ nomUbigeo
+					+ "</a>"
+					+ "</td></tr>";
+					//Pinta Lista de Ubigeos By idCampania/idPeriodo/idTipPdv
+					$("#ubigeoLst").html(lista);
+				})
+			}
+		}
+	});
+}
+
+function ubigeoPinta(idUbigeo, nomUbigeo){
+	$("#idUbigeo_ins").val(idUbigeo);
+	$("#ubigeo_ins").val(nomUbigeo);
+	$("#dlg_ubigeo_find").dialog("close");
 }
